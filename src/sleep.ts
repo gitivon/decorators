811b3ea021241f1bb5f9ee8fd83c8dc779e0a38e
ivonhua <ivonhua@vipabc.com>
2018-04-14 14:47:55 +0800

    add ts-lint

--- src/sleep.ts
+++ src/sleep.ts
@@ -1,12 +1,14 @@
 import n from './middleware';
 
-const timeout = (t: number): Promise<void> => new Promise(r => {
-  // @ts-ignore
-  setTimeout(r, t); 
-});
+const timeout = (t: number): Promise<void> =>
+  new Promise(r => setTimeout(r, t));
 
-export const sleep = (t: number) => (target: any, name: string, descriptor: PropertyDescriptor): PropertyDescriptor => n({target, name, descriptor}, async (next) => {
-  await timeout(t);
-  await next();
-});
-
+export const sleep = (t: number) => (
+  target: any,
+  name: string,
+  descriptor: PropertyDescriptor
+): PropertyDescriptor =>
+  n({ target, name, descriptor }, async next => {
+    await timeout(t);
+    await next();
+  });
